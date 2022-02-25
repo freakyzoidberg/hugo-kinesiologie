80837c9b8883f08de7031b535faa257a28b8e4c8
Pierre <pierre@lacave.me>
2022-02-25 16:18:11 +0100

    catch parse error

--- netlify/functions/send-email/send-email.js
+++ netlify/functions/send-email/send-email.js
@@ -7,30 +7,30 @@ const API_KEY = process.env.MAILGUN_API_KEY;
 const DOMAIN = "kinesiologiereims.fr"
 
 const handler = async (event) => {
-  const body = JSON.parse(event.body)
-  const client = mailgun.client({username: 'api', key: API_KEY});
 
-  const messageData = {
-    from: 'no-reply@kinesiologiereims.fr',
-    'h:Reply-To': body.email,
-    to: 'marielle.legoff.pro@gmail.com',
-    bcc: 'pierre@lacave.me',
-    subject: `Kinesiologie: message de ${body.name}`,
-    text: `${body.text} \n\n\nmail: ${body.email}\ntel: ${body.tel}`
-  };
-  
-  try {
-    await client.messages.create(DOMAIN, messageData)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-    return { statusCode: 200, body: '' }
-  } catch (error) {
-    return { statusCode: 500, body: error.message }
-  }
+    try {
+	const body = JSON.parse(event.body)
+	const client = mailgun.client({username: 'api', key: API_KEY});
+	
+	const messageData = {
+	    from: 'no-reply@kinesiologiereims.fr',
+	    'h:Reply-To': body.email,
+	    to: 'marielle.legoff.pro@gmail.com',
+	    bcc: 'pierre@lacave.me',
+	    subject: `Kinesiologie: message de ${body.name}`,
+	    text: `${body.text} \n\n\nmail: ${body.email}\ntel: ${body.tel}`
+	};
+	await client.messages.create(DOMAIN, messageData)
+	    .then((res) => {
+		console.log(res);
+	    })
+	    .catch((err) => {
+		console.error(err);
+	    });
+	return { statusCode: 200, body: '' }
+    } catch (error) {
+	return { statusCode: 500, body: error.message }
+    }
 }
 
 module.exports = { handler }
